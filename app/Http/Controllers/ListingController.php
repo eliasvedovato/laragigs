5ac4008cef7686e7b04503b60323b785e4bdb9c0
eliasvedovato <eliasvedovato@gmail.com>
2022-07-11 19:31:33 -0300

    File upload

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -11,7 +11,7 @@ class ListingController extends Controller
     // Show all listings
     public function index() {
         return view('listings.index', [
-            'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(2)
+            'listings' => Listing::latest()->filter(request(['tag', 'search']))->paginate(6)
         ]);
     }
 
@@ -38,6 +38,10 @@ class ListingController extends Controller
             'tags' => 'required',
             'description' => 'required'
         ]);
+
+        if($request->hasFile('logo')) {
+            $formFields['logo'] = $request->file('logo')->store('logos', 'public');
+        }
 
         Listing::create($formFields);
 
